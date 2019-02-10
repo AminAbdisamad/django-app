f30f355e44841f00a8ae0104d7497de9881df5f6
Amin Abdisamad <theaminux@gmail.com>
2019-02-10 06:02:33 +0300

    updated

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name="user-login"),
+    path('', views.login, name="login"),
     path('register/', views.register, name="register")
 ]
