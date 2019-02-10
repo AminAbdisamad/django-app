99f1eda1255f622617153d2037583c80988fdbe7
Amin Abdisamad <theaminux@gmail.com>
2019-02-10 07:16:05 +0300

    arranged some stuff

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,17 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-# Import views for login and logout
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('pages/', include('pages.urls')),
     # Create blog home page without redirect
     path('', include('pages.urls')),
     path('users/', include('users.urls')),
-    path('login/', auth_views.LoginView.as_view(
-        template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(
-        template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
-
+# Import views for login and logout
+from django.contrib.auth import views as auth_views
+from . import views as user_views
 urlpatterns = [
-    path('', views.login, name="login"),
-    path('register/', views.register, name="register"),
-    path('profile/', views.profile, name="profile")
+    path('', user_views.login, name="login"),
+    path('register/', user_views.register, name="register"),
+    path('profile/', user_views.profile, name="profile"),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='users/logout.html'), name='logout'),
 ]
